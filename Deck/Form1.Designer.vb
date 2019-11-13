da0b18c6b02f5337780577cb7d4554bca0b08364
megamaxie <57732648+megamaxie@users.noreply.github.com>
2019-11-13 22:36:21 +0000

    Add files via upload

---
+++ Deck/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblCard = New System.Windows.Forms.Label()
+        Me.btnLower = New System.Windows.Forms.Button()
+        Me.btnHigher = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnSame = New System.Windows.Forms.Button()
+        Me.tbCall = New System.Windows.Forms.TextBox()
+        Me.btnCall = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblCard
+        '
+        Me.lblCard.AutoSize = True
+        Me.lblCard.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCard.Location = New System.Drawing.Point(385, 116)
+        Me.lblCard.Name = "lblCard"
+        Me.lblCard.Size = New System.Drawing.Size(0, 29)
+        Me.lblCard.TabIndex = 0
+        '
+        'btnLower
+        '
+        Me.btnLower.Location = New System.Drawing.Point(140, 259)
+        Me.btnLower.Name = "btnLower"
+        Me.btnLower.Size = New System.Drawing.Size(75, 61)
+        Me.btnLower.TabIndex = 2
+        Me.btnLower.Text = "Lower"
+        Me.btnLower.UseVisualStyleBackColor = True
+        '
+        'btnHigher
+        '
+        Me.btnHigher.Location = New System.Drawing.Point(140, 171)
+        Me.btnHigher.Name = "btnHigher"
+        Me.btnHigher.Size = New System.Drawing.Size(75, 61)
+        Me.btnHigher.TabIndex = 3
+        Me.btnHigher.Text = "Higher"
+        Me.btnHigher.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Cooper Black", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(317, 269)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 32)
+        Me.lblResult.TabIndex = 4
+        '
+        'btnSame
+        '
+        Me.btnSame.BackColor = System.Drawing.Color.Yellow
+        Me.btnSame.Font = New System.Drawing.Font("Copperplate Gothic Bold", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSame.ForeColor = System.Drawing.Color.Red
+        Me.btnSame.Location = New System.Drawing.Point(12, 326)
+        Me.btnSame.Name = "btnSame"
+        Me.btnSame.Size = New System.Drawing.Size(383, 115)
+        Me.btnSame.TabIndex = 5
+        Me.btnSame.Text = "SAME (Do You Dare?)"
+        Me.btnSame.UseVisualStyleBackColor = False
+        '
+        'tbCall
+        '
+        Me.tbCall.Location = New System.Drawing.Point(130, 56)
+        Me.tbCall.Name = "tbCall"
+        Me.tbCall.Size = New System.Drawing.Size(100, 22)
+        Me.tbCall.TabIndex = 6
+        '
+        'btnCall
+        '
+        Me.btnCall.Location = New System.Drawing.Point(140, 84)
+        Me.btnCall.Name = "btnCall"
+        Me.btnCall.Size = New System.Drawing.Size(75, 61)
+        Me.btnCall.TabIndex = 7
+        Me.btnCall.Text = "Call"
+        Me.btnCall.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(327, 17)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Call a number (10 is Jack, 11 is Queen, 13 is King)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1080, 503)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCall)
+        Me.Controls.Add(Me.tbCall)
+        Me.Controls.Add(Me.btnSame)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnHigher)
+        Me.Controls.Add(Me.btnLower)
+        Me.Controls.Add(Me.lblCard)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblCard As Label
+    Friend WithEvents btnLower As Button
+    Friend WithEvents btnHigher As Button
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnSame As Button
+    Friend WithEvents tbCall As TextBox
+    Friend WithEvents btnCall As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Deck/Form1.vb
@@ -0,0 +1,141 @@
+﻿Public Class Form1
+    Dim sSuits(0 To 3) As String
+    Dim sCards(0 To 12) As String
+    Dim cGuess As Int16
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+
+        'Assigns the suits
+        sSuits(0) = "Spades"
+        sSuits(1) = "Hearts"
+        sSuits(2) = "Clubs"
+        sSuits(3) = "Diamonds"
+
+        'Assigns the cards
+        sCards(0) = "Ace"
+        sCards(1) = "Two"
+        sCards(2) = "Three"
+        sCards(3) = "Four"
+        sCards(4) = "Five"
+        sCards(5) = "Six"
+        sCards(6) = "Seven"
+        sCards(7) = "Eight"
+        sCards(8) = "Nine"
+        sCards(9) = "Ten"
+        sCards(10) = "Jack"
+        sCards(11) = "Queen"
+        sCards(12) = "King"
+        CardDraw()
+
+    End Sub
+
+    Private Sub btnHigher_Click(sender As Object, e As EventArgs) Handles btnHigher.Click
+        Dim rndmSuit As Int16
+        Dim rndmCard As Int16
+
+        rndmSuit = Int((3 * Rnd()))
+        rndmCard = Int((12 * Rnd()))
+        lblCard.Text = (sCards(rndmCard) & " of " & sSuits(rndmSuit))
+
+
+        If cGuess > rndmCard Then
+            lblResult.Text = "Drink!"
+        ElseIf cGuess = rndmCard Then
+            lblResult.Text = "Everybody Drink!"
+        ElseIf cGuess < rndmCard Then
+            lblResult.Text = "You're Safe!"
+        End If
+        cGuess = rndmCard
+    End Sub
+    Private Sub btnLower_Click(sender As Object, e As EventArgs) Handles btnLower.Click
+        Dim rndmSuit As Int16
+        Dim rndmCard As Int16
+
+        rndmSuit = Int((3 * Rnd()))
+        rndmCard = Int((12 * Rnd()))
+        lblCard.Text = (sCards(rndmCard) & " of " & sSuits(rndmSuit))
+
+
+        If cGuess < rndmCard Then
+            lblResult.Text = "Drink!"
+        ElseIf cGuess = rndmCard Then
+            lblResult.Text = "Everybody Drink!"
+        ElseIf cGuess > rndmCard Then
+            lblResult.Text = "You're Safe!"
+        End If
+
+        cGuess = rndmCard
+
+    End Sub
+
+    Private Sub CardDraw()
+        Dim rndmSuit As Int16
+        Dim rndmCard As Int16
+
+        rndmSuit = Int((3 * Rnd()))
+        rndmCard = Int((12 * Rnd()))
+        lblCard.Text = (sCards(rndmCard) & " of " & sSuits(rndmSuit))
+        cGuess = rndmCard
+    End Sub
+
+    Private Sub btnDraw_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnSame_Click(sender As Object, e As EventArgs) Handles btnSame.Click
+        Dim rndmSuit As Int16
+        Dim rndmCard As Int16
+
+        rndmSuit = Int((3 * Rnd()))
+        rndmCard = Int((12 * Rnd()))
+        lblCard.Text = (sCards(rndmCard) & " of " & sSuits(rndmSuit))
+
+
+        If cGuess = rndmCard Then
+            lblResult.Text = "EVERYONE DOWN YOUR DRINKS!"
+        ElseIf cGuess <> rndmCard Then
+            lblResult.Text = "DOWN YOUR DRINK BITCH"
+        End If
+
+        cGuess = rndmCard
+    End Sub
+
+    Private Sub btnCall_Click(sender As Object, e As EventArgs) Handles btnCall.Click
+
+        Dim rndmSuit As Int16
+        Dim rndmCard As Int16
+        If tbCall.Text <> "" Then
+            cGuess = Convert.ToInt64(tbCall.Text) - 1
+            rndmSuit = Int((3 * Rnd()))
+            rndmCard = Int((12 * Rnd()))
+            lblCard.Text = (sCards(rndmCard) & " of " & sSuits(rndmSuit))
+
+
+            If cGuess = rndmCard Then
+                lblResult.Text = "EVERYONE TAKE 3 DRINKS!"
+                btnHigher.Enabled = True
+                btnLower.Enabled = True
+                btnSame.Enabled = True
+            ElseIf cGuess + 1 = rndmCard Then
+                lblResult.Text = "AH FUCK, 3 drinks and the next player MUST guess a card!"
+                btnHigher.Enabled = False
+                btnLower.Enabled = False
+                btnSame.Enabled = False
+            ElseIf cGuess - 1 = rndmCard Then
+                lblResult.Text = "AH FUCK, 3 drinks and the next player MUST guess a card!"
+                btnHigher.Enabled = False
+                btnLower.Enabled = False
+                btnSame.Enabled = False
+            ElseIf cGuess <> rndmCard Then
+                lblResult.Text = "TAKE 3 DRINKS!"
+                btnHigher.Enabled = True
+                btnLower.Enabled = True
+                btnSame.Enabled = True
+            End If
+        Else
+            MessageBox.Show("Make a call pussy!")
+        End If
+        cGuess = rndmCard
+    End Sub
+End Class
